3f710b8e7c0238c93ef4fb09f07631a5ad7e1f5e
mmmichael-carvalho <162504810+mmmichael-carvalho@users.noreply.github.com>
2025-10-24 22:04:34 -0300

    update

--- frontend/src/app/services/quiz.service.ts
+++ frontend/src/app/services/quiz.service.ts
@@ -1,5 +1,5 @@
 
-import { environment } from '../../environment';
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
