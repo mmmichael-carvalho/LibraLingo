824cd3a3b25b298ffabfcc0b12ddce0f03cf8f5d
mmmichael-carvalho <162504810+mmmichael-carvalho@users.noreply.github.com>
2025-10-27 13:42:14 -0300

    update

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://libralingo.onrender.com/api'
+};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://libralingo.onrender.com/api'
+  production: false,
+  apiUrl: 'http://localhost:8080/api'
 };
